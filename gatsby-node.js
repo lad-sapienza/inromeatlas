c86f3f54c0c195e3b90bf5f5effe5bf06f19df27
Eleonora Iacopini <eleonora.iacopini@gmail.com>
2025-01-19 23:09:26 +0100

    Double language IT - EN

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,20 @@
 const path = require("path")
-const postTemplate = path.resolve(`./src/templates/contents.jsx`)
+const { createFilePath } = require("gatsby-source-filesystem")
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === "Mdx") {
+    const slug =
+      node.frontmatter.slug ||
+      createFilePath({ node, getNode, basePath: "contents" })
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug.startsWith("/") ? slug : `/${slug}`,
+    })
+  }
+}
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
@@ -10,9 +25,12 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
         nodes {
           id
           frontmatter {
-            description
-            slug
             title
+            language
+            slug
+          }
+          fields {
+            slug
           }
           internal {
             contentFilePath
@@ -26,17 +44,46 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     reporter.panicOnBuild("Error loading MDX result", result.errors)
   }
 
-  // Create blog post pages.
   const posts = result.data.allMdx.nodes
 
   posts.forEach(node => {
-    const pagePath =
-      node.frontmatter.slug === "home" ? "/" : node.frontmatter.slug
+    const languagePrefix = node.frontmatter.language
+      ? `/${node.frontmatter.language}`
+      : ""
+    let pagePath
+
+    // Homepage inglese gestita come "/" e "/en/home"
+    if (
+      node.frontmatter.slug === "home" &&
+      node.frontmatter.language === "en"
+    ) {
+      createPage({
+        path: `/en/home`,
+        component: `${path.resolve(
+          "./src/templates/contents.jsx",
+        )}?__contentFilePath=${node.internal.contentFilePath}`,
+        context: {
+          id: node.id,
+          language: node.frontmatter.language,
+        },
+      })
+
+      // Alias per "/"
+      pagePath = "/"
+    } else {
+      // Percorso normale per tutte le altre pagine
+      pagePath = `${languagePrefix}${node.fields.slug}`
+    }
 
     createPage({
-      path: pagePath,
-      component: `${postTemplate}?__contentFilePath=${node.internal.contentFilePath}`,
-      context: { id: node.id },
+      path: pagePath.replace(/\/\//g, "/"), // Evita doppie barre
+      component: `${path.resolve(
+        "./src/templates/contents.jsx",
+      )}?__contentFilePath=${node.internal.contentFilePath}`,
+      context: {
+        id: node.id,
+        language: node.frontmatter.language,
+      },
     })
   })
 }
--- src/modules/autoNavbar.js
+++ src/modules/autoNavbar.js
@@ -3,6 +3,7 @@ import { Container, Nav, Navbar } from "react-bootstrap"
 import { withPrefix } from "gatsby"
 import styled from "styled-components"
 import { useStaticQuery, graphql } from "gatsby"
+import { navigate } from "gatsby"
 
 function AutoNavbar(props) {
   const data = useStaticQuery(graphql`
@@ -17,23 +18,68 @@ function AutoNavbar(props) {
             slug
             title
             menu_position
+            language
           }
         }
       }
     }
   `)
+
+  // Ottieni la lingua corrente dall'URL
+  const currentLanguage =
+    typeof window !== "undefined"
+      ? window.location.pathname.split("/")[1] || "en"
+      : "en"
+
+  // Filtra le voci di menu per lingua corrente
+  const filteredMenuItems = data.allMdx.nodes.filter(
+    menuItem => menuItem.frontmatter.language === currentLanguage,
+  )
+
+  const handleLanguageChange = lang => {
+    const currentPath = window.location.pathname.replace(/\/$/, "") // Rimuove eventuale "/" finale
+    const currentSlug = currentPath.split("/").pop() // Ottieni lo slug della pagina corrente
+
+    let newSlug
+
+    // Cerca lo slug della pagina nella lingua selezionata
+    const correspondingPage = data.allMdx.nodes.find(
+      menuItem =>
+        menuItem.frontmatter.language === lang &&
+        menuItem.frontmatter.slug === currentSlug,
+    )
+
+    if (correspondingPage) {
+      // Usa lo slug della pagina corrispondente
+      newSlug = correspondingPage.frontmatter.slug
+    } else {
+      // Se non trovi una pagina corrispondente, usa "home" per entrambe le lingue
+      newSlug = "home"
+    }
+
+    // Costruisci il nuovo percorso
+    const newPath = lang === "en" ? `/en/${newSlug}` : `/it/${newSlug}`
+
+    // Rimuovi doppie barre "//" se presenti
+    navigate(newPath.replace(/\/\//g, "/"))
+  }
+
   return (
     <Menu>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href={withPrefix(`/`)}>{props.siteTitle}</Navbar.Brand>
+          <Navbar.Brand href={withPrefix(`/${currentLanguage}/`)}>
+            {props.siteTitle}
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              {data.allMdx.nodes.map((menuItem, index) => (
+              {filteredMenuItems.map((menuItem, index) => (
                 <div className="containerLink" key={index}>
                   <Nav.Link
-                    href={withPrefix(`/${menuItem.frontmatter.slug}`)}
+                    href={withPrefix(
+                      `/${menuItem.frontmatter.language}/${menuItem.frontmatter.slug}`,
+                    )}
                     className="nav-item my-2"
                   >
                     {menuItem.frontmatter.title}
@@ -41,6 +87,20 @@ function AutoNavbar(props) {
                 </div>
               ))}
             </Nav>
+            <div className="language-switcher">
+              <button
+                className={`btn ${currentLanguage === "en" ? "btn-primary" : "btn-outline-primary"}`}
+                onClick={() => handleLanguageChange("en")}
+              >
+                EN
+              </button>
+              <button
+                className={`btn ${currentLanguage === "it" ? "btn-primary" : "btn-outline-primary"}`}
+                onClick={() => handleLanguageChange("it")}
+              >
+                IT
+              </button>
+            </div>
           </Navbar.Collapse>
         </Container>
       </Navbar>
@@ -52,6 +112,12 @@ function AutoNavbar(props) {
 const Menu = styled.div`
   .bg-body-tertiary {
     background-color: #ececec !important;
+  }
+
+  .language-switcher {
+    display: flex;
+    gap: 10px;
+    margin-left: auto;
   }
 `
 export default AutoNavbar
