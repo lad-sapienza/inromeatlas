6b4c48877f2cce06d61fca5538d5751ff84bd782
Eleonora Iacopini <eleonora.iacopini@gmail.com>
2025-01-19 23:09:43 +0100

    Slide only in homepage

--- src/usr/layout/layout.js
+++ src/usr/layout/layout.js
@@ -1,12 +1,13 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-
 import { Container } from "react-bootstrap"
 
 import Navbar from "../../modules/autoNavbar"
 import Footer from "./footer"
 import Header from "./header"
 import "./layout.scss"
+
+const Slide = React.lazy(() => import("./slide")) // Lazy load dello slide
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -19,12 +20,30 @@ const Layout = ({ children }) => {
     }
   `)
 
+  const [isHomePage, setIsHomePage] = React.useState(false)
+
+  React.useEffect(() => {
+    // Controlla se il percorso è la homepage in inglese o italiano
+    const pathname = window.location.pathname.replace(/\/$/, "") // Rimuovi slash finale
+    const isHome =
+      pathname === "" ||
+      pathname === "/" ||
+      pathname === "/en/home" ||
+      pathname === "/it/home"
+
+    setIsHomePage(isHome)
+  }, []) // Esegui il controllo una sola volta al montaggio
+
   return (
     <>
       <div className="container-fluid p-0">
-        <Navbar siteTitle={data.site.siteMetadata?.title || `Title`} />
-        
         <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+        <Navbar siteTitle={data.site.siteMetadata?.title || `Title`} />
+        {isHomePage && (
+          <React.Suspense fallback={<div>Loading slide...</div>}>
+            <Slide />
+          </React.Suspense>
+        )}
         <main>
           <Container>{children}</Container>
         </main>
