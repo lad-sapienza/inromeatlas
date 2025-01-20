72f628fad170225ffa651f62089c0f8f448a844c
Eleonora Iacopini <eleonora.iacopini@gmail.com>
2025-01-20 02:45:03 +0100

    Update footer.js

--- src/usr/layout/footer.js
+++ src/usr/layout/footer.js
@@ -2,55 +2,103 @@ import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import styled from "styled-components"
 import { Link } from "gatsby"
-import { Container } from "react-bootstrap"
+import { Container, Row, Col } from "react-bootstrap"
+import { withPrefix } from "gatsby"
 
 const FooterSection = () => {
   return (
     <Footer>
-      <Container>
-        <div className="d-flex flex-row align-items-center mb-3">
-          <div className="p-3">
-            <Link to="/">
+      <Container fluid>
+        <Row className="px-5 pt-5">
+          <Col sm={2} xs={12} className="d-flex ">
+            <div>
+              <Link href="/">
+                <StaticImage
+                  src="../../images/INRome_logo_def.png"
+                  width={140}
+                  quality={30}
+                  formats={["AUTO", "WEBP"]}
+                  alt="InRome"
+                  className="img-fluid"
+                />
+              </Link>
+              <p className="footer-title">© Scuola Normale Superiore</p>
+            </div>
+          </Col>
+
+          <Col sm={2} xs={6} className="d-flex justify-content-end">
+            <Link href="https://www.sns.it/it" target="_blank" rel="noreferrer">
               <StaticImage
-                src="../../images/scms-lad.png"
-                width={100}
-                quality={80}
+                src="../../images/logo-sns@2x_0.png"
+                height={71}
+                quality={30}
                 formats={["AUTO", "WEBP"]}
-                alt="LAD: Laboratorio di Archeologia Digitale alla Sapienza"
-                className="img-fluid"
+                alt="SNS"
               />
             </Link>
-          </div>
-          <div className="p-3 border-start border-primary">
-            <p className="p-s-3">
-              <strong>s:CMS</strong> è un progetto ideato e sviluppato dal{" "}
-              <br />
-              <a
-                href="https://lad.saras.uniroma1.it"
-                target="_blank"
-                rel="noreferrer"
-              >
-                LAD: Laboratorio di Archeologia Digitale alla Sapienza
-              </a>
-              <br />
-              <a
-                href="https://github.com/lab-archeologia-digitale/sCMS"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Code
-              </a>
-              &nbsp;|&nbsp;
-              <a
-                href="https://github.com/lab-archeologia-digitale/sCMS/issues"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Issues
-              </a>
-            </p>
-          </div>
-        </div>
+          </Col>
+          <Col sm={2} xs={6} className="d-flex justify-content-end">
+            <Link
+              href="https://www.uniroma1.it/it/pagina-strutturale/home"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <StaticImage
+                src="../../images/sapienza.png"
+                height={71}
+                quality={30}
+                formats={["AUTO", "WEBP"]}
+                alt="Sapienza Università di Roma"
+              />
+            </Link>
+          </Col>
+          <Col sm={2} xs={6} className="d-flex justify-content-end">
+            <Link
+              href="https://www.ait.ac.at/en/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <StaticImage
+                src="../../images/ait-logo_blog.svg"
+                height={71}
+                quality={30}
+                formats={["AUTO", "WEBP"]}
+                alt="AIT"
+              />
+            </Link>
+          </Col>
+          <Col sm={2} xs={6} className="d-flex justify-content-end">
+            <Link
+              href="https://erc.europa.eu/homepage"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <StaticImage
+                src="../../images/eu-erc.png"
+                height={71}
+                quality={30}
+                formats={["AUTO", "WEBP"]}
+                alt="EU"
+              />
+            </Link>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={4} xs={12} className="d-flex justify-content-start"></Col>
+          <Col
+            sm={4}
+            xs={12}
+            className="d-flex Link-footer justify-content-center"
+          >
+            <Link to={withPrefix("en/contact")} className="footer-link">
+              CONTACT{" "}
+            </Link>
+            <Link to={withPrefix("en/credit")} className="footer-link">
+              CREDIT{" "}
+            </Link>
+          </Col>
+          <Col sm={4} xs={12} className="d-flex justify-content-end"></Col>
+        </Row>
       </Container>
     </Footer>
   )
@@ -58,12 +106,45 @@ const FooterSection = () => {
 
 //style
 const Footer = styled.footer`
-  background-color: #ececec;
-  border-top: #000 solid 0.5rem;
-  min-height: auto;
-  margin-top: 3rem;
-  padding-top: 1rem;
-  padding-bottom: 1rem;
+  .container-fluid {
+    background-color: #8b5a40;
+    border-bottom: #3e281c solid 1rem;
+    min-height: auto;
+  }
+
+  a {
+    color: #fff;
+    text-decoration: none;
+    font-size: 0.7rem;
+  }
+  a:hover {
+    color: #000;
+    text-decoration: none;
+    font-size: 0.7rem;
+  }
+  .footer-link {
+    color: #fff;
+    text-decoration: none;
+    font-size: 0.7rem;
+    padding: 0 2rem 0 2rem;
+  }
+  .footer-link:hover {
+    color: #000;
+    text-decoration: none;
+    font-size: 0.7rem;
+    padding: 0 2rem 0 2rem;
+  }
+
+  .footer-title {
+    color: #fff;
+    font-family: "Cinzel";
+    font-size: 0.6rem;
+  }
+  .Link-footer {
+    width: 100%;
+    text-align: center;
+    padding: 20px;
+  }
 `
 
 export default FooterSection
