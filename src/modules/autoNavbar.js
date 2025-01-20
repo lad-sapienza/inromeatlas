fef57d56df4620c81917e4b1c072c4682afa41c5
Eleonora Iacopini <eleonora.iacopini@gmail.com>
2025-01-20 02:51:43 +0100

    Update autoNavbar.js

--- src/modules/autoNavbar.js
+++ src/modules/autoNavbar.js
@@ -122,13 +122,86 @@ function AutoNavbar(props) {
 
 const Menu = styled.div`
   .bg-body-tertiary {
-    background-color: #ececec !important;
+    background-color: #8b5a40 !important;
+    padding: 0;
+    border-top: 20px solid #3e281c;
+  }
+  .nav-item {
+    padding: 0;
+    margin: 0;
+    color: #fff;
+    border-right: 1px solid white;
   }
 
+  .nav-item:hover {
+    padding: 0;
+    margin: 0;
+    color: #000;
+    border-right: 1px solid white;
+  }
+
+  .containerLink:hover {
+    background-color: #a99e58;
+  }
+
+  .nav-link {
+    padding: 0;
+    width: 110px;
+    text-align: center;
+  }
+
+  #basic-nav-dropdown {
+    color: #fff;
+  }
+
+  .dropdown-menu {
+    background-color: #a99e58;
+    color: #fff;
+    padding-left: 10px;
+    border-radius: 0;
+    border: none !important;
+    margin: 0 !important;
+  }
+
+  .dropdown-item {
+    border: 0 !important;
+  }
+  .dropdown-item:hover {
+    background-color: #fff;
+    color: #000;
+  }
   .language-switcher {
     display: flex;
+    align-items: center;
     gap: 10px;
     margin-left: auto;
+  }
+
+  .language-switcher .btn {
+    padding: 5px 10px;
+    font-size: 0.8rem;
+    border-radius: 3px;
+    text-transform: uppercase;
+  }
+
+  .language-switcher .btn-primary {
+    background-color: #a99e58;
+    border-color: #a99e58;
+  }
+
+  .language-switcher .btn-primary:hover {
+    background-color: #8b5a40;
+    border-color: #8b5a40;
+  }
+
+  .language-switcher .btn-outline-primary {
+    color: #a99e58;
+    border-color: #a99e58;
+  }
+
+  .language-switcher .btn-outline-primary:hover {
+    background-color: #a99e58;
+    color: #fff;
   }
 `
 
